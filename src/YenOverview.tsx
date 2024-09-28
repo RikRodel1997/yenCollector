42d1f6711963044ad632be70a16f9b135e2eac6c
RikRodel1997 <origin1997bf4@gmail.com>
2024-09-28 09:46:11 +0200

    Made a simple Text based overview

--- src/YenOverview.tsx
+++ src/YenOverview.tsx
@@ -1,29 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { Text, FlatList } from "react-native";
-import { openDb, getData } from "./Database";
 
 interface YenOverviewProps {
     records: YenRecord[];
 }
 
 export const YenOverview: React.FC<YenOverviewProps> = ({ records }) => {
+    const coins = [1, 5, 10, 50, 100, 500];
+
     return (
         <>
-            <Text>Overview</Text>
+            <Text>Overview{"\n"}</Text>
             <Text>
                 You've collected{" "}
                 {records.filter((record) => record.inPossession).length} coins
             </Text>
-            <FlatList
-                data={records}
-                renderItem={({ item }) => (
-                    <Text>
-                        {item.eraJp} {item.eraEn} {item.yenValue}{" "}
-                        {item.inPossession} {item.condition}
-                    </Text>
+            <Text>
+                You're missing{" "}
+                {records.filter((record) => !record.inPossession).length} coins
+                {"\n"}
+            </Text>
+            {coins.map((coin, index) => renderCoin(records, coin))}
+        </>
+    );
+};
+
+const renderCoin = (records: YenRecord[], coin: number) => {
+    const recordsByCoin = records.filter((record) => record.yenValue === coin);
+    return (
+        <>
+            <Text>
+                {coin} yen coins -{" "}
+                {Math.round(
+                    (recordsByCoin.filter((record) => record.inPossession)
+                        .length /
+                        recordsByCoin.length) *
+                        100
                 )}
-                keyExtractor={(item) => item.id.toString()}
-            />
+                %
+            </Text>
+            <Text>
+                Collected:{" "}
+                {recordsByCoin.filter((record) => record.inPossession).length}
+            </Text>
+            <Text>
+                Missing:{" "}
+                {recordsByCoin.filter((record) => !record.inPossession).length}
+                {"\n"}
+            </Text>
         </>
     );
 };
